81c5fb93092c8eb0bf1b264a7ff1034287e6282c
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-04-22 00:42:19 -0700

    Add recipes to item page

--- XIVCat/Util.cs
+++ XIVCat/Util.cs
@@ -7,4 +7,9 @@ public static class Util
         var folder = (Math.Floor(iconId / 1000d) * 1000).ToString("000000");
         return $"https://xivapi.com/i/{folder}/{iconId:000000}.png";
     }
+
+    public static string GetItemPageUrl(uint itemId)
+    {
+        return $"item/{itemId}";
+    }
 }
