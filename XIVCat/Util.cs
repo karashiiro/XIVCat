cd156625d4eb63ddd94f97a43592b287ed423407
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-04-21 20:03:27 -0700

    Add item page

---
+++ XIVCat/Util.cs
@@ -0,0 +1,10 @@
+﻿namespace XIVCat;
+
+public static class Util
+{
+    public static string GetIconUrl(uint iconId)
+    {
+        var folder = (Math.Floor(iconId / 1000d) * 1000).ToString("000000");
+        return $"https://xivapi.com/i/{folder}/{iconId:000000}.png";
+    }
+}
